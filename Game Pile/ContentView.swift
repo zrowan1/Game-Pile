af9e3d797c8420a61542a2562f3da1346b04747b
rowandeschaux <483907@student.fontys.nl>
2022-03-30 10:36:29 +0200

    Initial Commit

---
+++ Game Pile/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  Game Pile
+//
+//  Created by Rowan Deschaux on 09/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+        
+    var body: some View {
+        
+        TabView
+        {
+            ZStack
+            {
+                Text("You have\n X\ngames in your collection")
+                    .padding()
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .background(.orange)
+                    .clipShape(RoundedRectangle(cornerRadius: 40))
+                    .padding(.bottom, 500)
+                
+                Text("Something for you?\n\n")
+                    .padding()
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .background(.orange)
+                    .clipShape(RoundedRectangle(cornerRadius: 40))
+                    
+                
+                Button {
+                    
+                } label: {
+                    Text("ADD GAME")
+                        .foregroundColor(.black)
+                }
+                .padding()
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .background(.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 40))
+                .padding(.top, 500)
+            }
+            .tabItem
+            {
+                Image(systemName: "arrowtriangle.right.circle")
+            }
+            GamesView()
+            .tabItem
+            {
+                Image(systemName: "gamecontroller")
+            }
+            StatsView()
+            .tabItem
+            {
+                Image(systemName: "square.grid.3x3.fill")
+            }
+            SettingsView()
+            .tabItem
+            {
+                Image(systemName: "gear")
+            }
+
+        }
+        .onAppear() {
+            UITabBar.appearance().barTintColor = .white
+        }
+        .accentColor(Color.orange)
+
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game Pile/Game_PileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_PileApp.swift
+//  Game Pile
+//
+//  Created by Rowan Deschaux on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Game_PileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
